5dec88dce65bca2c2eef1076b901e9a5e60d232b
діана <d.r.riabovol@student.khai.edu>
2025-12-23 19:26:43 +0200

    ChatBot 2-nd stage

---
+++ chatbot.py
@@ -0,0 +1,10 @@
+def greet():
+    bot_name = "DICT_Bot"
+    birth_year = 2025
+
+    print(f"Hello! My name is {bot_name}.")
+    print(f"I was created in {birth_year}.")
+def remind_name():
+    print(f"Please remind me youre name.")
+    name = input()
+    print(f"What a great name you have, {name}")
