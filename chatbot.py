bc8fd05310d9765691e9c7df6ba4aaa3eede2235
діана <d.r.riabovol@student.khai.edu>
2025-12-23 19:28:02 +0200

    ChatBot 4-nd stage

--- chatbot.py
+++ chatbot.py
@@ -17,3 +17,10 @@ def remind_name():
 
     age = (remainder3 * 70 + remainder5 * 21 + remainder7 * 15) % 105
     print(f"Your age is {age}; that's a good time to start programming!")
+    print("Now I will prove to you that I can count to any number you want.")
+    number = int(input("> "))
+
+    for i in range(number + 1):
+        print(f"{i} !")
+
+    print("Completed, have a nice day!")
